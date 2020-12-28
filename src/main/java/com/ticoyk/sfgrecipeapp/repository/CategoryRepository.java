37c55f662e6a14f19ba989dfd9b27032ea94d1e4
TicoYk <didimoykym@gmail.com>
2020-12-28 18:09:53 -0300

    Added capability of finding by description

--- src/main/java/com/ticoyk/sfgrecipeapp/repository/CategoryRepository.java
+++ src/main/java/com/ticoyk/sfgrecipeapp/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.ticoyk.sfgrecipeapp.repository;
 
+import java.util.Optional;
+
 import com.ticoyk.sfgrecipeapp.domain.Category;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long>{
-    
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/com/ticoyk/sfgrecipeapp/repository/UnitOfMeasureRepository.java
+++ src/main/java/com/ticoyk/sfgrecipeapp/repository/UnitOfMeasureRepository.java
@@ -1,9 +1,11 @@
 package com.ticoyk.sfgrecipeapp.repository;
 
+import java.util.Optional;
+
 import com.ticoyk.sfgrecipeapp.domain.UnitOfMeasure;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
-    
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
