2bbabe7deb7e362428b80651ae0bb6c2feb5860a
TicoYk <didimoykym@gmail.com>
2020-12-30 19:40:45 -0300

    Removed Unnecessary imports

--- src/main/java/com/ticoyk/sfgrecipeapp/domain/Category.java
+++ src/main/java/com/ticoyk/sfgrecipeapp/domain/Category.java
@@ -4,13 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
-import org.hibernate.annotations.FetchProfile;
 
 import lombok.Data;
 
--- src/main/java/com/ticoyk/sfgrecipeapp/domain/Ingredient.java
+++ src/main/java/com/ticoyk/sfgrecipeapp/domain/Ingredient.java
@@ -3,7 +3,6 @@ package com.ticoyk.sfgrecipeapp.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
